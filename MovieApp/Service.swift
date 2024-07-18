e6aacf5f02ed554b040dd8bae89a5e463003f6ec
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-07-18 16:09:32 +0300

    API Call function completed

--- MovieApp/Service.swift
+++ MovieApp/Service.swift
@@ -13,7 +13,29 @@ class Service {
         let apiKey = "fbf719d2"
         let urlString = "http://www.omdbapi.com/?apikey=\(apiKey)&t=\(title)"
         
+        guard let url = URL(string: urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "") else {
+            completion(.failure(NSError(domain: "Invalid Url", code: -1, userInfo: nil)))
+            return
+        }
         
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: -1, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let movie = try JSONDecoder().decode(Movie.self, from: data)
+                completion(.success(movie))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
         
     }
 }
